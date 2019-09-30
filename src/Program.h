d9ad9ac2dd54628d51d4db03adc1d07ca2e764c8
Meysam Kazemi <kazemii.meysam@gmail.com>
2019-09-29 20:48:58 -0600

    initial commit

---
+++ src/Program.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/transform.hpp>
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+#include "Geometry.h"
+#include "InputHandler.h"
+#include "RenderEngine.h"
+
+class Program {
+
+public:
+	Program();
+	void start();
+
+private:
+	GLFWwindow* window;
+	RenderEngine* renderEngine;
+
+	std::vector<Geometry*> geometryObjects;
+
+	bool show_test_window;
+	ImVec4 clear_color;
+
+	static void error(int error, const char* description);
+	void setupWindow();
+	void mainLoop();
+	void drawUI();
+
+	void createTestGeometryObject();
+	void createTestGeometryObject1();
+};
